c4a9c2346af06555bfff3807b5e73c966998102a
Saksham Agrawal <40560369+saksham115@users.noreply.github.com>
2025-10-17 23:14:40 +0530

    format fix

--- extension/fts/test/prepare_test.cpp
+++ extension/fts/test/prepare_test.cpp
@@ -7,10 +7,10 @@ namespace testing {
 
 TEST_F(ApiTest, PrepareFTSTest) {
 #ifndef __STATIC_LINK_EXTENSION_TEST__
-    ASSERT_TRUE(conn->query(common::stringFormat("LOAD EXTENSION '{}'",
-                                TestHelper::appendRyuRootPath(
-                                    "extension/fts/build/libfts.ryu_extension")))
-                    ->isSuccess());
+    ASSERT_TRUE(
+        conn->query(common::stringFormat("LOAD EXTENSION '{}'",
+                        TestHelper::appendRyuRootPath("extension/fts/build/libfts.ryu_extension")))
+            ->isSuccess());
 #endif
     ASSERT_TRUE(
         conn->query("CALL CREATE_FTS_INDEX('person', 'personIdx', ['fName'])")->isSuccess());
--- extension/fts/test/read_only_test.cpp
+++ extension/fts/test/read_only_test.cpp
@@ -11,10 +11,10 @@ TEST_F(ApiTest, ReadOnlyDBTest) {
     }
     createDBAndConn();
 #ifndef __STATIC_LINK_EXTENSION_TEST__
-    ASSERT_TRUE(conn->query(common::stringFormat("LOAD EXTENSION '{}'",
-                                TestHelper::appendRyuRootPath(
-                                    "extension/fts/build/libfts.ryu_extension")))
-                    ->isSuccess());
+    ASSERT_TRUE(
+        conn->query(common::stringFormat("LOAD EXTENSION '{}'",
+                        TestHelper::appendRyuRootPath("extension/fts/build/libfts.ryu_extension")))
+            ->isSuccess());
 #endif
     ASSERT_TRUE(conn->query("CREATE NODE TABLE doc (NAME STRING, PRIMARY KEY(NAME))")->isSuccess());
     ASSERT_TRUE(conn->query("CREATE (d:doc {NAME: 'alice'})")->isSuccess());
@@ -23,10 +23,10 @@ TEST_F(ApiTest, ReadOnlyDBTest) {
     systemConfig->readOnly = true;
     createDBAndConn();
 #ifndef __STATIC_LINK_EXTENSION_TEST__
-    ASSERT_TRUE(conn->query(common::stringFormat("LOAD EXTENSION '{}'",
-                                TestHelper::appendRyuRootPath(
-                                    "extension/fts/build/libfts.ryu_extension")))
-                    ->isSuccess());
+    ASSERT_TRUE(
+        conn->query(common::stringFormat("LOAD EXTENSION '{}'",
+                        TestHelper::appendRyuRootPath("extension/fts/build/libfts.ryu_extension")))
+            ->isSuccess());
 #endif
     ASSERT_STREQ("Connection exception: Cannot execute write operations in a read-only database!",
         conn->query("CALL CREATE_FTS_INDEX('doc', 'docIdx1', ['NAME'])")->toString().c_str());
